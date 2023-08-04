8a4985e99aa572718e453ac573e17817fc9d4d71
Froussios <darkmatter.tm@gmail.com>
2023-08-04 23:48:05 +1000

    Show context on notes-context

--- scripts/elements.ts
+++ scripts/elements.ts
@@ -1,9 +1,37 @@
-import { LitElement, html } from "lit";
-import { customElement } from "lit/decorators";
+import { LitElement, PropertyValueMap, html } from "lit";
+import { customElement, state, property } from "lit/decorators";
 
-@customElement("notes-test")
-export class Test extends LitElement {
+/**
+ *
+ * @returns the active tabs.
+ */
+export function QueryContext(): Promise<chrome.tabs.Tab[]> {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true }, (result: chrome.tabs.Tab[]) => {
+      resolve(result);
+    });
+  });
+}
+
+@customElement("notes-context")
+export class Context extends LitElement {
+  @property() url = "Loading";
+
   render() {
-    return html`<span>testtest</span>`;
+    return html`<h2>${this.url}</h2>`;
+  }
+
+  /** Show the active tab url. */
+  async refreshContext() {
+    const tabs = await QueryContext();
+    this.url = tabs.filter((tab) => !!tab.url)[0].url!;
+  }
+
+  firstUpdated() {
+    this.refreshContext();
+
+    setInterval(() => {
+      this.refreshContext();
+    }, 2000);
   }
 }
