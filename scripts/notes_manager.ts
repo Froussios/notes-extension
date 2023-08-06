dc657c4dec6dee59cb1a600e1959217f50dcfc7a
Froussios <darkmatter.tm@gmail.com>
2023-08-06 15:40:47 +1000

    Insert new note at the top

--- scripts/notes_manager.ts
+++ scripts/notes_manager.ts
@@ -31,7 +31,7 @@ export class NoteManager extends LitElement {
       "",
       this.currentUrl || "Invalid"
     );
-    this.notes.push(newNote);
+    this.notes.unshift(newNote);
     this.requestUpdate();
   }
 
