3471110a50b989d44f235cfbc182738a2a4955c1
Christos <cfroussios@gmail.com>
2024-06-15 16:45:07 +1000

    Don't upload every function every time

--- cloud_functions/deploy_function.sh
+++ cloud_functions/deploy_function.sh
@@ -8,6 +8,10 @@ mkdir -p build/function_source/
 cp -r build/tsc/ build/function_source/
 cp package.json package-lock.json build/function_source/
 
-gcloud functions deploy postNotes --region=europe-west6 --project=notes-extension-425902 --source=build/function_source/ --trigger-http --runtime=nodejs20 --memory=256Mi --gen2 --allow-unauthenticated
+if [[ $# -lt 1 || $1 == "postNotes" ]]; then
+  gcloud functions deploy postNotes --region=europe-west6 --project=notes-extension-425902 --source=build/function_source/ --trigger-http --runtime=nodejs20 --memory=256Mi --gen2 --allow-unauthenticated
+fi
 
-gcloud functions deploy getNotes --region=europe-west6 --project=notes-extension-425902 --source=build/function_source/ --trigger-http --runtime=nodejs20 --memory=256Mi --gen2 --allow-unauthenticated
+if [[ $# -lt 1 || $1 == "getNotes" ]]; then
+  gcloud functions deploy getNotes --region=europe-west6 --project=notes-extension-425902 --source=build/function_source/ --trigger-http --runtime=nodejs20 --memory=256Mi --gen2 --allow-unauthenticated
+fi
